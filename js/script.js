146e3a693edd1b05587f38e58e67180787c8f34d
Suju <108076874+Sujuforny@users.noreply.github.com>
2022-12-26 16:24:04 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
+
+
+
+function change_image(image){
+
+  var container = document.getElementById("main-image");
+
+ container.src = image.src;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+
+
+
+
+});
